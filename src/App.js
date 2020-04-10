0bfb75bdedadaed005ea02a9cb54baf1d6f41385
kmsimmons <kmsimmons19@gmail.com>
2020-04-10 18:29:59 +0200

    it works!

--- src/App.js
+++ src/App.js
@@ -1,41 +1,79 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+const formatInput = (value, prevVal) => {
+  if(!value) return value
+  const currentVal = value.replace(/[^\d]/g, '')
+  const currentValLength = currentVal.length
+
+  if(!prevVal || value.length > prevVal.length) {
+    
+    //first three
+    if(currentValLength < 4)
+      return currentVal
+    
+    //add parentheses
+    if(currentValLength < 7)
+      return `(${currentVal.slice(0, 3)}) ${currentVal.slice(3)}`
+
+    //add dash
+    return `(${currentVal.slice(0, 3)}) ${currentVal.slice(3, 6)}-${currentVal.slice(6, 10)}`
+  }
+}
+
+const validateInput = value => {
+  let error = ''
+
+  if(!value) error = 'Required'
+  if(value.length !== 14) error = 'Please enter a valid phone number, ex: (123)456-7890'
+  return error
+}
+
+export default class App extends Component {
+  constructor() {
+    super()
     this.state = {
       number: '',
-      regexp: /^[0-9\b]+$/
+      error: ''
     }
     this.onNumberChange = this.onNumberChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  onNumberChange = e => {
-    let number = e.target.value
+  onNumberChange({ target: { value } }) {
+    this.setState(prevState => ({ number: formatInput(value, prevState.number)}))
+  }
 
-    if(number === '' || this.state.regexp.test(number)) {
-      this.setState({ [e.target.name]: number})
-    }
+  handleSubmit(e) {
+    e.preventDefault()
+    const error = validateInput(this.state.number)
+    this.setState({ error }, () => {
+      if(!error) {
+        setTimeout(() => {
+          alert(JSON.stringify(this.state, null,  4))
+        }, 300)
+      }
+    })
   }
 
   render() {
      return (
       <div className='App'>
-        <h1>Please Enter Your Phone Number</h1>
-        <input
-          type='tel'
-          name='number'
-          placeholder='(555)555-5555'
-          value={this.state.number}
-          onChange={this.onNumberChange}>
-        </input>
-        <button>
-          Submit
-        </button>
+        <form onSubmit={this.handleSubmit}>
+          <h1>Please Enter Your Phone Number</h1>
+          <input
+            type='text'
+            name='number'
+            placeholder='(555)555-5555'
+            value={this.state.number}
+            onChange={this.onNumberChange}>
+          </input>
+          <button>
+            Submit
+          </button>
+        </form>
       </div>
     )
   }
 }
 
-export default App;
---
+++ src/other.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+
+const normalizeInput = (value, previousValue) => {
+  if (!value) return value;
+  const currentValue = value.replace(/[^\d]/g, '');
+  const cvLength = currentValue.length;
+  
+  if (!previousValue || value.length > previousValue.length) {
+    if (cvLength < 4) return currentValue;
+    if (cvLength < 7) return `(${currentValue.slice(0, 3)}) ${currentValue.slice(3)}`;
+    return `(${currentValue.slice(0, 3)}) ${currentValue.slice(3, 6)}-${currentValue.slice(6, 10)}`;
+  }
+};
+
+const validateInput = value => {
+  let error = ""
+  
+  if (!value) error = "Required!"
+  else if (value.length !== 14) error = "Invalid phone format. ex: (555) 555-5555";
+  
+  return error;
+};
+    
+export default class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = { phone: "", error: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  
+  handleChange({ target: { value } }) {   
+    this.setState(prevState=> ({ phone: normalizeInput(value, prevState.phone) }));
+  };
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    const error = validateInput(this.state.phone);
+    
+    this.setState({ error }, () => {
+       if(!error) {
+         setTimeout(() => {
+           alert(JSON.stringify(this.state, null, 4));
+         }, 300)
+       }
+    });
+  }
+  
+  handleReset() {
+     this.setState({ phone: "", error: "" });
+  };
+  
+  render() {
+    return(
+      <form className="form" onSubmit={this.handleSubmit}>
+        <div className="input-container">
+          <p className="label">Phone:</p>
+          <input
+            className="input"
+            type="text"
+            name="phone"
+            placeholder="(xxx) xxx-xxxx"
+            value={this.state.phone}
+            onChange={this.handleChange}
+          />
+          {this.state.error && <p className="error">{this.state.error}</p>}
+        </div>
+        <div className="btn-container">
+          <button 
+             className="btn danger"
+             type="button"
+             onClick={this.handleReset}
+           >
+            Reset
+          </button>
+          <button className="btn primary" type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
+}
